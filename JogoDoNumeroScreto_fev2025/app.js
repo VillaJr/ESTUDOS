ee1c5a8c26b09998d0bf9ef4848df58c5b8b9d33
Vila Junior <jrvilela16@gmail.com>
2025-02-25 21:12:49 -0300

    Função reiniciar funcionando

--- JogoDoNumeroScreto_fev2025/app.js
+++ JogoDoNumeroScreto_fev2025/app.js
@@ -1,17 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML = 'Mais um jogo do n° secreto';
-/*O método querySelector é usado para selecionar o primeiro elemento no documento que corresponde ao seletor especificado.
-No caso, o seletor é 'h1', que significa que estamos procurando a primeira tag <h1> no documento HTML.
-Se existir uma tag <h1>, ela será retornada e armazenada na variável titulo. Se não existir, titulo será null.
-Declaramos a variável tiulo e o resultado da seleção (querySelector) é armazenado nessa variável.*/
-
-//let paragrafo = document.querySelector('p');
-//paragrafo.innerHTML = 'Escolha um n° entre 1 e 10';
-/*innerHTML é uma propriedade do elemento HTML que permite acessar ou alterar o conteúdo interno desse elemento. 
-No caso, como paragrafo é uma referência a um elemento <p>, innerHTML se refere ao texto ou HTML que está dentro desse parágrafo.
-*/
 
 exibirMensagemInicial();
 
@@ -20,7 +8,7 @@ function exibirTextoNaTela(tag,texto) {
     campo.innerHTML = texto;
 }
 
-function exibirMensagemInicial(){
+function exibirMensagemInicial() {
     exibirTextoNaTela('h1','Jogo do número secreto');
     exibirTextoNaTela('p','Escolha um número entre 1 e 10');
 }
@@ -29,7 +17,7 @@ function exibirMensagemInicial(){
 function verificarChute() {
     let chute = document.querySelector('input').value;
     if(chute == numeroSecreto) {
-        exibirTextoNaTela('h1', 'Parabéns!');
+        exibirTextoNaTela('h1','Parabéns!');
         let palavraTentativa = tentativas > 1 ?'tentativas':'tentativa';
 
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
@@ -51,35 +39,15 @@ function verificarChute() {
 function gerarNumeroAleatorio(){
     return parseInt( Math.random() * 10 + 1 );
 }
-function limparCampo(){
+function limparCampo() {
     chute = document.querySelector('input');
     chute.value = "";
 }
 
-function reiniciarJogo(){
+function reiniciarJogo() {
     numeroSecreto = gerarNumeroAleatorio();
     limparCampo();
-    tentativas = 1;
+    tentativas =1;
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled',true);
 }
-
-
-/*function verificarChute():
-
-Aqui, estamos criando uma função chamada verificarChute.
-
-Uma função é um bloco de código que pode ser "chamado" ou executado sempre que necessário. Neste caso, a função foi criada para realizar uma tarefa específica.
-
-{ ... }:
-
-As chaves { } delimitam o corpo da função, ou seja, o código que será executado quando a função for chamada.
-
-console.log('O botão foi clicado!');:
-
-Dentro da função, temos uma única instrução: console.log().
-
-console.log() é uma função do JavaScript usada para exibir mensagens no console do navegador (ou do ambiente onde o código está sendo executado).
-
-A mensagem que será exibida é 'O botão foi clicado!'.*/
-
